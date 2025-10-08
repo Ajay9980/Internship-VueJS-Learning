efc535349b358de92d01390cdda48a9e39385a3a
Ajay9980 <a95313943@gmail.com>
2025-10-08 13:52:24 +0530

    week-1-day-3-Slot-Topic

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import week1Day1 from '../views/week-1-day-1&2.vue';
 import week1Assignment from '../views/week-1-Assignment.vue';
 import week1Day3 from '../views/week-1-day-3.vue';
-import week1Day31 from '../views/week-1-day-3.1.vue';
+import week1Day31 from '../views/week-1-Day-3.1-slot.vue';
 </script>
 
 
